889d71a6135a6fc16d5ec51247fd79aa1cb80389
Blade <bladerboles@gmail.com>
2022-11-25 15:26:04 -0700

    feat: setup and first commit, write outline of portfolio landing page

---
+++ app.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Projects</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+  <main>
+    <section>
+      <h1>Hello, World!</h1>
+      <h2>I'm <span>Blade</span>.</h2>
+      <p>
+        I write code and live in Boulder, CO. I enjoy full stack JavaScript and
+        TypeScript development.
+      </p>
+      <button>View Work</button>
+    </section>
+    <section>
+      <h2>About Me</h2>
+      <p>
+        I've been working as a full stack developer for the last 3 years. Before
+        that I was a chemist who dreamt of developing software. Coding makes me
+        feel like a wizard, and my 5 year plan revolves around becoming Gandalf.
+        The beard might take a little longer.
+      </p>
+      <p>
+        When I'm not writing code I'm gaming, reading, struggling to ride a
+        snowboard or adventuring with my partner and our Shiba Inu. I believe
+        that a sense of humor and a sense of purpose are two of the most
+        important things a person can have in life.
+      </p>
+    </section>
+    <section>
+      <h2>Completed Projects:</h2>
+      <p>
+        I removed all of my old portfolio projects because they no longer
+        reflect my skill level. Over the last couple of months I've been working
+        on some really fun projects after work. Be sure to check back in the
+        near future to see what I've been up to.
+      </p>
+      <p>
+        Feel free to <button>Contact Me</button> to hear about my professional
+        experience or the side projects I've been working on.
+      </p>
+    </section>
+    <footer>
+      <ul>
+        <li>Email</li>
+        <li>Phone</li>
+        <li>LinkedIn</li>
+      </ul>
+    </footer>
+  </main>
+</template>
+
+<style scoped></style>
