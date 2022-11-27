eb31761ddb271c95ed932dcaaef51bb77a4ad309
Blade <bladerboles@gmail.com>
2022-11-27 10:39:18 -0700

    feat: no more bootstrap, better styling

--- app.vue
+++ app.vue
@@ -1,58 +1,46 @@
 <template>
-  <nav
-    id="portfolio__navbar"
-    class="navbar navbar-expand-md navbar-dark bg-dark px-3"
-  >
-    <a class="navbar-brand" href="#">BB</a>
+  <nav class="portfolio__navbar">
+    <!-- <a class="navbar-brand" href="#">BB</a>
     <button
-      class="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarScroll"
-      aria-controls="navbarScroll"
-      aria-expanded="false"
       aria-label="Toggle navigation"
     >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarScroll">
-      <ul
-        class="nav-pills navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll nav"
-        style="--bs-scroll-height: 100px"
-      >
-        <li class="nav-item">
-          <a class="nav-link" href="#portfolio__greeting">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#portfolio__bio">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#portfolio__projects">Projects</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#portfolio__contact">Contact</a>
-        </li>
-      </ul>
-    </div>
+      <span>Toggle Icon</span>
+    </button> -->
+    <ul class="portfolio__navlist">
+      <li class="portfolio__navlist-item">
+        <a class="portfolio__navlist-anchor" href="#portfolio__greeting"
+          >Home</a
+        >
+      </li>
+      <li class="portfolio__navlist-item">
+        <a class="portfolio__navlist-anchor" href="#portfolio__bio">About</a>
+      </li>
+      <li class="portfolio__navlist-item">
+        <a class="portfolio__navlist-anchor" href="#portfolio__projects"
+          >Projects</a
+        >
+      </li>
+      <li class="portfolio__navlist-item">
+        <a class="portfolio__navlist-anchor" href="#portfolio__contact"
+          >Contact</a
+        >
+      </li>
+    </ul>
   </nav>
-  <main
-    data-bs-spy="scroll"
-    tabindex="0"
-    data-bs-offset="0"
-    data-bs-target="#portfolio__navbar"
-    class="portfolio__main"
-  >
-    <section id="portfolio__greeting">
-      <h1>Hello, World!</h1>
-      <h2>I'm <span>Blade</span>.</h2>
-      <p>
-        I write code and live in Boulder, CO. I enjoy full stack JavaScript and
-        TypeScript development.
-      </p>
-      <button>View Work</button>
+  <main class="portfolio__main">
+    <section
+      class="portfolio__section portfolio__section--greeting"
+      id="portfolio__greeting"
+    >
+      <h1>Hello, world!</h1>
+      <h2 class="portfolio__name-header">
+        I'm <span class="portfolio__name-span">Blade</span>.
+      </h2>
+      <p>I like full stack TypeScript development.</p>
+      <button class="portfolio__work-button">View Work</button>
     </section>
-    <section id="portfolio__bio">
-      <h2>About Me</h2>
+    <section class="portfolio__section" id="portfolio__bio">
+      <h2 class="portfolio__section-header">About Me</h2>
       <p>
         I've been working as a full stack developer for the last 3 years. Before
         that I was a chemist who dreamt of developing software. Coding makes me
@@ -66,8 +54,8 @@
         important things a person can have in life.
       </p>
     </section>
-    <section id="portfolio__projects">
-      <h2>Completed Projects:</h2>
+    <section class="portfolio__section" id="portfolio__projects">
+      <h2 class="portfolio__section-header">Completed Projects:</h2>
       <p>
         I removed all of my old portfolio projects because they no longer
         reflect my skill level. Over the last couple of months I've been working
@@ -79,7 +67,7 @@
         experience or the side projects I've been working on.
       </p>
     </section>
-    <footer id="portfolio__contact">
+    <footer class="portfolio__section" id="portfolio__contact">
       <ul>
         <li>Email</li>
         <li>Phone</li>
@@ -89,8 +77,89 @@
   </main>
 </template>
 
-<style scoped>
-.portfolio__main {
-  position: relative;
+<style>
+body {
+  background-color: #150811;
+  color: #e3dbdb;
+  font-family: roboto-slab;
+}
+
+p {
+  font-size: 22px;
+}
+
+section {
+  min-height: 100vh;
+}
+
+h1 {
+  padding-top: 15vh;
+}
+
+.portfolio__section-header {
+  padding-top: 15vh;
+}
+
+.portfolio__navbar {
+  position: fixed;
+  top: 0;
+  height: 15vh;
+  width: 100vw;
+  text-align: center;
+  vertical-align: middle;
+  z-index: 9999;
+  background-color: #150811;
+}
+
+.portfolio__navlist {
+  position: absolute;
+  top: 25%;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100%;
+}
+
+.portfolio__navlist-item {
+  display: inline;
+  margin: 0 5%;
+}
+
+.portfolio__navlist-anchor {
+  text-decoration: none;
+  color: inherit;
+  font-size: 22px;
+}
+
+.portfolio__navlist-anchor:hover {
+  color: #d1462f;
+}
+
+.portfolio__section--greeting {
+  justify-content: center;
+  margin-top: -15vh;
+}
+
+.portfolio__section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.portfolio__name-header {
+  margin: 20px 0;
+}
+
+.portfolio__name-span {
+  color: #d1462f;
+  font-weight: 500;
+}
+
+.portfolio__work-button {
+  color: inherit;
+  background-color: inherit;
+  border: 3px solid #d1462f;
+  padding: 10px 20px;
 }
 </style>
