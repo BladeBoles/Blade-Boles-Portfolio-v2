1796c4b5764ac03ad79463e1ca3281b487261d31
Blade <bladerboles@gmail.com>
2022-12-05 19:33:53 -0700

    feat: get favicon and hamburger menu nav links working

--- app.vue
+++ app.vue
@@ -9,6 +9,15 @@
     </button>
     <ul v-if="navOpen" class="portfolio__navlist--dropdown">
       <li class="portfolio__navlist-item">
+        <button
+          @click="toggleNav"
+          class="portfolio__nav-close"
+          aria-label="Toggle navigation"
+        >
+          <close-rounded />
+        </button>
+      </li>
+      <li class="portfolio__navlist-item">
         <a class="portfolio__navlist-anchor" href="#portfolio__greeting"
           >Home</a
         >
@@ -58,12 +67,9 @@
       <h2 class="portfolio__name-header">
         I'm <span class="portfolio__name-span">Blade</span>.
       </h2>
-      <p>I'm a full stack developer. I really like TypeScript.</p>
-      <button class="portfolio__work-button">
-        <a class="portfolio__work-button-anchor" href="#portfolio__projects"
-          >View Work</a
-        >
-      </button>
+      <p class="portfolio__personal-headline">
+        I'm a full stack developer. I really like JavaScript and TypeScript.
+      </p>
     </section>
     <section class="portfolio__section" id="portfolio__bio">
       <h2 class="portfolio__section-header">About Me</h2>
@@ -96,19 +102,20 @@
       <div class="portfolio__section-projects">
         <p class="portfolio__section-paragraph">
           I removed all of my old portfolio projects because they no longer
-          reflect my skill level.
-          <a
-            href="https://app.pivotinteractives.com/activities"
-            class="portfolio__work-preview"
-            >Click here</a
-          >
-          if you'd like a peak at the work project I've been on for the last 2
-          years. Check back in the near future to see what I've been working on
-          in my spare time.
+          reflect my skill level. Check back in the near future to see what I've
+          been working on in my spare time.
         </p>
         <p class="portfolio__section-paragraph">
           Feel free to contact me to hear more about my professional experience
-          or the side projects I've got going that aren't ready to release yet.
+          or the side projects I have that aren't ready to release yet. You can
+          also check out my
+          <a
+            href="https://github.com/BladeBoles/"
+            class="portfolio__github-link"
+            target="_blank"
+            >GitHub</a
+          >
+          to get an idea of what I've been up to.
         </p>
       </div>
     </section>
@@ -123,22 +130,19 @@
           >
         </li>
         <li class="portfolio__footer--socials">
-          <p>
-            <a href="#"
-              ><img
-                class="portfolio__footer--gh-image"
-                src="./assets/GitHub-Mark-Light-64px.png"
-                alt=""
-            /></a>
-          </p>
-          <p>
-            <a href="#"
-              ><img
-                class="portfolio__footer--li-image"
-                src="./assets/LI-In-Bug.png"
-                alt=""
-            /></a>
-          </p>
+          <a target="_blank" href="https://github.com/BladeBoles/"
+            ><img
+              class="portfolio__footer--gh-image"
+              src="./assets/GitHub-Mark-Light-64px.png"
+              alt=""
+          /></a>
+
+          <a target="_blank" href="https://www.linkedin.com/in/blade-boles/"
+            ><img
+              class="portfolio__footer--li-image"
+              src="./assets/In-White-128-Γäó@2x.png"
+              alt=""
+          /></a>
         </li>
       </ul>
     </footer>
@@ -147,16 +151,21 @@
 <script setup>
 import { ref } from 'vue'
 import FaBars from '~icons/fa6-solid/bars'
-
+import CloseRounded from '~icons/material-symbols/close-rounded'
+useHead({
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+})
 const navOpen = ref(false)
 
 const toggleNav = () => (navOpen.value = !navOpen.value)
 </script>
 <style>
+* {
+  font-family: roboto-slab;
+}
 body {
   background-color: #150811;
   color: #e3dbdb;
-  font-family: roboto-slab;
 }
 
 p {
@@ -171,6 +180,10 @@ h1 {
   padding-top: 15vh;
 }
 
+.portfolio__personal-headline {
+  text-align: center;
+}
+
 .portfolio__nav-toggle {
   position: absolute;
   top: 0;
@@ -178,9 +191,23 @@ h1 {
   z-index: 10000;
   background-color: inherit;
   color: inherit;
-  font-size: 36px;
+  font-size: 40px;
   border: none;
   margin-right: 10px;
+}
+
+.portfolio__nav-close {
+  color: inherit;
+  font-size: 28px;
+  background-color: transparent;
+  border: none;
+  align-self: flex-end;
+  margin-right: -5px;
+  padding: 0;
+}
+
+.portfolio__nav-close:hover {
+  color: #d1462f;
 }
 
 .portfolio__section-header {
@@ -188,13 +215,13 @@ h1 {
 }
 
 .portfolio__navlist--dropdown {
+  width: 100%;
   display: flex;
   flex-direction: column;
+  align-items: flex-end;
   position: fixed;
   top: 0;
   right: 0;
-  margin-top: 64px;
-  margin-right: 10px;
   z-index: 10000;
   background-color: #150811;
   padding: 0;
@@ -204,12 +231,16 @@ h1 {
 .portfolio__navbar--small {
   position: fixed;
   top: 0;
-  height: 100px;
+  height: 64px;
   width: 100vw;
   text-align: center;
   vertical-align: middle;
   z-index: 9999;
   background-color: #150811;
+}
+
+.portfolio__navbar--small {
+  height: 64px;
 }
 
 .portfolio__navlist {
@@ -302,21 +333,24 @@ h1 {
   margin-top: 50px;
   max-width: 90vw;
 }
-.portfolio__work-preview {
+
+.portfolio__github-link {
   color: inherit;
 }
-.portfolio__work-preview:hover {
+.portfolio__github-link:hover {
   color: #d1462f;
   text-decoration: 2px #d1462f underline;
 }
 .portfolio__footer {
   width: 80%;
   display: flex;
+  flex-direction: column;
   list-style-type: none;
   margin: 0;
   padding: 0;
   font-size: 22px;
-  justify-content: space-between;
+  justify-content: center;
+  align-items: center;
 }
 
 .portfolio__footer--email-span {
@@ -334,8 +368,14 @@ h1 {
 
 .portfolio__footer--socials {
   display: flex;
-  width: 25%;
-  justify-content: space-between;
+  margin: 30px 0;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+
+.portfolio__footer--gh-image {
+  margin-right: 30px;
 }
 
 .portfolio__footer--gh-image,
@@ -366,6 +406,9 @@ h1 {
   }
   .portfolio__bio-headshot {
     margin-right: 0;
+  }
+  .portfolio__section-header {
+    padding-top: 20vh;
   }
 }
 </style>
